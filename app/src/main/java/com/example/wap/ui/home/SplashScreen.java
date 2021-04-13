1b1d3ec70f2fa31abd4707bf7a64ffd323b9acea
Josh Lavallee <44557318+jlavallee76@users.noreply.github.com>
2021-04-13 12:05:40 -0500

    Added home about fragments and implemented liquid swipe

--- app/src/main/java/com/example/wap/ui/home/SplashScreen.java
+++ app/src/main/java/com/example/wap/ui/home/SplashScreen.java
@@ -1,20 +1,31 @@
 package com.example.wap.ui.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.example.wap.AboutFragment1;
+import com.example.wap.AboutFragment2;
+import com.example.wap.AboutFragment3;
 import com.example.wap.MainActivity;
 import com.example.wap.R;
 
 public class SplashScreen extends AppCompatActivity {
 
-    ImageView logo, appName, splashBackground;
+    ImageView logo, splashBackground;
     LottieAnimationView lottieAnimationView;
 
     @Override
@@ -26,9 +37,9 @@ public class SplashScreen extends AppCompatActivity {
         splashBackground = findViewById(R.id.splash_bg);
         lottieAnimationView = findViewById(R.id.sea_waves_animation);
 
-        splashBackground.animate().translationY(2500).setDuration(1000).setStartDelay(4000);
-        logo.animate().translationY(1425).setDuration(1000).setStartDelay(4000);
-        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        splashBackground.animate().setDuration(1000).setStartDelay(4000);
+        logo.animate().setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().setDuration(1000).setStartDelay(4000);
 
         new Handler().postDelayed(new Runnable() {
             @Override
